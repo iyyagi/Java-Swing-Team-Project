8944b4e798f110cb8b5013ab74c0d97493b2600e
ByungCheon-Kang <115913274+iyyagi@users.noreply.github.com>
2023-03-15 19:41:13 +0900

    Update DBTest.java

--- JavaTeamProject/src/pos/receipt_check/DBTest.java
+++ JavaTeamProject/src/pos/receipt_check/DBTest.java
@@ -23,21 +23,15 @@ public class DBTest {
 				+ "   test2 VARCHAR2(255))";
 		
 		try (
-				Connection conn = OjdbcConnection.getConnection();
-				PreparedStatement pstmt1 = conn.prepareStatement(sql1);
-		){
-			// Connection 인스턴스는 처음에 기본적으로 자동 커밋이 활성화 되어있다
-			conn.setAutoCommit(false); //이거 왜하는?
+			Connection conn = OjdbcConnection.getConnection();
+			PreparedStatement pstmt1 = conn.prepareStatement(sql1);
+			){
 
-			
-			// Insert, Update, Delete는 executeUpdate() 메서드를 사용한다 결과는 int가 나옴
 			int row = pstmt1.executeUpdate();
 			
 			System.out.println(row + "행이 성공적으로 추가되었습니다");
 			
 			System.out.println(row + "행이 성공적으로 100원 올랐습니다");
-			
-			//Connection 객체에 커밋과 롤백이 있다
 			
 			System.out.println("savepoint1로 돌아가 update만 취소되었습니다.");
 			
@@ -46,7 +40,6 @@ public class DBTest {
 			System.out.println("commit을 통해 변경 사항을 확정 지었습니다");
 			
 			} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
